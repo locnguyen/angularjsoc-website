f2fb73c5eee8263ba822f4301403a98c041fb848
Phi Dong <phidong@gmail.com>
2013-08-04 15:07:12 -0700

    change meetup API call to return plain text; remove removehtml filter, add truncate filter

--- js/angularjsoc.js
+++ js/angularjsoc.js
@@ -17,3 +17,22 @@ angular.module('filters', []).
                 return String(text).replace(/<\/?([a-z][a-z0-9]*)\b[^>]*>/gi, '').substring(0,250)+'...';
     }
 });
+
+angular.module('filters', []).
+    filter('truncate', function () {
+        return function (text, length, end) {
+            if (isNaN(length))
+                length = 250;
+
+            if (end === undefined)
+                end = "...";
+
+            if (text.length <= length || text.length - end.length <= length) {
+                return text;
+            }
+            else {
+                return String(text).replace(/<\/?([a-z][a-z0-9]*)\b[^>]*>/gi, '').substring(0, length-end.length) + end;
+            }
+
+        };
+    });
--- js/controllers.js
+++ js/controllers.js
@@ -3,10 +3,8 @@
 /* Controllers */
 
 function UpcomingMeetups($scope, $http) {
-  $http.jsonp('https://api.meetup.com/2/events?&sign=true&group_id=918203&page=20&sign=true&key=18224834563235331b1963378625522&callback=JSON_CALLBACK').success(function(data) {
+  $http.jsonp('https://api.meetup.com/2/events?&sign=true&group_id=918203&page=20&sign=true&key=18224834563235331b1963378625522&text_format=plain&callback=JSON_CALLBACK').success(function(data) {
     $scope.events = data.results;
   });
-
-  $scope.sortBy = 'date';
 }
 
