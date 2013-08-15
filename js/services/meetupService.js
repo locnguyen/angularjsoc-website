324a74cd0bda65472c1d9c7980da37d665e4c700
Loc Nguyen <lochnguyen@gmail.com>
2013-08-14 23:27:18 -0700

    Updated GROUP_ID to angularjsoc's for launch

--- js/services/meetupService.js
+++ js/services/meetupService.js
@@ -1,7 +1,7 @@
 angularjsOC.app.factory('meetupService', ['$http', function($http) {
-    // GROUP_ID = 9218222 for AngularJSOC. Use  918203 for dev
+    // GROUP_ID = 9218222 for AngularJSOC. Use 918203 for dev
 
-    var GROUP_ID = 918203,
+    var GROUP_ID = 9218222,
         API_KEY = '5732f7612a68125a17145474123f';
 
     function url(str) {
